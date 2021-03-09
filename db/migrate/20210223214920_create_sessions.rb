d1f617e4a94ac2cdb7e9fd2e8bb7db24436e62a8
danajackson2 <danajackson2@gmail.com>
2021-03-09 12:39:00 -0800

    final commit

--- db/migrate/20210223214920_create_sessions.rb
+++ db/migrate/20210223214920_create_sessions.rb
@@ -2,7 +2,7 @@ class CreateSessions < ActiveRecord::Migration[6.1]
   def change
     create_table :sessions do |t|
       t.date :date
-      t.integer :duration
+      t.string :duration
       t.integer :focus_rating
       t.integer :prod_rating
       t.text :notes
